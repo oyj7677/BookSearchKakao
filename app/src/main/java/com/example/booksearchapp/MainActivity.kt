78780ca93d10f669f55ff3486912da512cb27d77
OYJ <oyj7677@gmail.com>
2024-05-06 21:15:30 +0900

    Feat: 바텀 네비게이션 적용
    1. 앱 첫 실행 시 Search Books 노출
    2. 바텀 네비게이션 클릭 시 fragment 변경
    3. mainActivity은 FrameLayout과 BottomNavigationView로 구성

--- app/src/main/java/com/example/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/example/booksearchapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.booksearchapp.databinding.ActivityMainBinding
+import com.example.booksearchapp.ui.view.FavoriteFragment
+import com.example.booksearchapp.ui.view.SearchFragment
+import com.example.booksearchapp.ui.view.SettingFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -12,5 +15,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupBottomNavigationView()
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.fragment_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
