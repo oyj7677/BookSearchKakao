ada0ffc2e4e437efb289c5edd25ffff07b52652f
OYJ <oyj7677@gmail.com>
2024-05-06 12:44:33 +0900

    Feat: 기본 프레그먼트 생성
    1. Search, Favorite, Setting 3개의 프레그먼트 생성
    2. viewBinding 적용

--- app/src/main/java/com/example/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/example/booksearchapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.booksearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.booksearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
