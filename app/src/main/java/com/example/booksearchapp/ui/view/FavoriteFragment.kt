784e2c8e8e134a6f63dfaa042e44a9c8cb13289c
OYJ <oyj7677@gmail.com>
2024-05-09 11:17:41 +0900

    Refactor : stateFlow 사용 시 발생하는 코루틴 보일러 플래이트 제거
    1. FavoriteFragment에 확장함수를 만들어서 사용.

--- app/src/main/java/com/example/booksearchapp/ui/view/FavoriteFragment.kt
+++ app/src/main/java/com/example/booksearchapp/ui/view/FavoriteFragment.kt
@@ -5,9 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -16,9 +13,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.booksearchapp.databinding.FragmentFavoriteBinding
 import com.example.booksearchapp.ui.adapter.BookSearchAdapter
 import com.example.booksearchapp.ui.viewmodel.BookSearchViewModel
+import com.example.booksearchapp.util.collectLatestStateFlow
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 
 class FavoriteFragment : Fragment() {
@@ -43,12 +39,8 @@ class FavoriteFragment : Fragment() {
         setupRecyclerView()
         setupTouchHelper(view)
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                bookSearchViewModel.favoriteBooks.collectLatest {
-                    bookSearchAdapter.submitList(it)
-                }
-            }
+        collectLatestStateFlow(bookSearchViewModel.favoriteBooks) {
+            bookSearchAdapter.submitList(it)
         }
     }
 
