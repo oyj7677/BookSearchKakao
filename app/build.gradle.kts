77bd5f1d693c81fbc1dff09b320aac309ad4280f
OYJ <oyj7677@gmail.com>
2024-05-13 16:11:44 +0900

    Refactor: build.gradle 리펙터링
    1. 카테고리 별 object 생성(defaultConfit, Dependencies, Plugins, Testing, Versions)
    2. 기존 build.gradle 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,23 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
-    id("androidx.navigation.safeargs.kotlin")
-    id("kotlin-parcelize")
-    id("dagger.hilt.android.plugin")
+    id(Plugins.ANDROID_APPLICATION)
+    id(Plugins.KOTLIN_ANDROID)
+    id(Plugins.KAPT)
+    id(Plugins.SECRETS_GRADLE_PLUGIN)
+    id(Plugins.SAFEARGS)
+    id(Plugins.PARCELIZE)
+    id(Plugins.HILT_PLUGIN)
 }
 
 android {
     namespace = "com.example.booksearchapp"
-    compileSdk = 34
+    compileSdk = DefaultConfig.COMPILE_SDK_VERSION
 
     defaultConfig {
         applicationId = "com.example.booksearchapp"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = DefaultConfig.MIN_SDK_VERSION
+        targetSdk = DefaultConfig.TARGET_SDK_VERSION
+        versionCode = DefaultConfig.VERSION_CODE
+        versionName = DefaultConfig.VERSION_NAME
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -50,71 +50,71 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.material)
+    implementation(Dependencies.constraintLayout)
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.retrofitConverterMoshi)
 
     // Moshi
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
+    implementation(Dependencies.moshi)
+    kapt(Dependencies.moshiKapt)
 
     // OKhttp
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(Dependencies.okhttp)
+    implementation(Dependencies.okhttpLoggingInterceptor)
 
     // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.7.0")
+    implementation(Dependencies.lifecycleViewmodelKtx)
+    implementation(Dependencies.lifecycleRuntimeKtx)
+    implementation(Dependencies.lifecycleViewmodelSavedstate)
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation(Dependencies.coroutinesCore)
+    implementation(Dependencies.coroutinesAndroid)
 
     // Coil
-    implementation("io.coil-kt:coil:2.5.0")
+    implementation(Dependencies.coil)
 
     // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation(Dependencies.recyclerview)
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation(Dependencies.navigationFragementKtx)
+    implementation(Dependencies.navigationUiKtx)
 
     // Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-paging:2.6.1")
+    implementation(Dependencies.roomRuntime)
+    implementation(Dependencies.roomKtx)
+    kapt(Dependencies.roomKapt)
+    implementation(Dependencies.roomPaging)
 
     // Kotlin serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation(Dependencies.kotlinSerialization)
 
     // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation(Dependencies.preferencesDatastore)
 
     // Paging
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+    implementation(Dependencies.paging)
 
     // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation(Dependencies.workmanager)
 
     // Dagger Hilt
-    implementation("com.google.dagger:hilt-android:2.46")
-    kapt("com.google.dagger:hilt-compiler:2.46")
+    implementation(Dependencies.daggerHilt)
+    kapt(Dependencies.daggerHiltKapt)
 
     // Hilt extensions
-    implementation("androidx.hilt:hilt-work:1.2.0")
-    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    implementation(Dependencies.hiltExtensionWork)
+    kapt(Dependencies.hitlExtensionKapt)
 
     // ViewModel delegate
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.7.0")
+    implementation(Dependencies.activityKtx)
+    implementation(Dependencies.fragmentKtx)
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,45 @@
+object Versions {
+    // Project
+    const val AGP = "8.2.1"
+    const val KOTLIN = "1.9.22"
+    const val SECRETS_GRADLE = "2.0.1"
+
+    // Androidx
+    const val CORE_KTX = "1.13.1"
+    const val APP_COMPAT = "1.6.1"
+    const val MATERIAL = "1.12.0"
+    const val CONSTRAINT_LAYOUT = "2.1.4"
+
+    // Testing
+    const val JUNIT4 = "4.13.2"
+    const val TEST_EXT_JUNIT = "1.1.5"
+    const val ESPRESSO_CORE = "3.5.1"
+    const val TEST_EXT_TRUTH = "1.4.0"
+    const val TEST_RUNNER = "1.4.0"
+    const val ROBOLETRIC = "4.8.1"
+    const val TEST_CORE = "1.4.0"
+    const val COROUTINES_TEST = "1.6.2"
+    const val ESPRESSO_CONTRIB = "3.4.0"
+    const val HAMCREST = "2.2"
+    const val HILT_ANDROID_TESTING = "2.41"
+    const val HILT_ANDROID_COMPILER = "2.41"
+
+    // Library
+    const val RETROFIT = "2.11.0"
+    const val MOSHI = "1.15.1"
+    const val OKHTTP = "4.12.0"
+    const val LIFECYCLE = "2.7.0"
+    const val COROUTINE = "1.7.3"
+    const val COIL = "2.5.0"
+    const val RECYCLERVIEW = "1.3.2"
+    const val NAVIGATION = "2.7.7"
+    const val ROOM = "2.6.1"
+    const val KOTLIN_SERIALIZATION = "1.6.3"
+    const val DATASTORE = "1.1.1"
+    const val PAGING = "3.2.1"
+    const val WORKMANGER = "2.9.0"
+    const val HILT = "2.46"
+    const val ACTIVITY_KTX = "1.9.0"
+    const val FRAGMENT_KTX = "1.7.0"
+    const val HILT_EXTENSION = "1.2.0"
+}
